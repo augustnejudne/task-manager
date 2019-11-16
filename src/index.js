cb30ca135c06b11275fd9b46e8c93a9eade1161c
August Nejudne <august@acudeen.com>
2019-11-16 16:38:27 +0800

    added index route

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 // routes
+app.get('/', (req, res) => res.send('Task manager app by Kim Nejudne'));
 app.use('/users', require('./routes/users.routes.js'));
 app.use('/tasks', require('./routes/tasks.routes.js'));
 
@@ -14,4 +15,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`task manager is listening on PORT: ${PORT}`);
 });
-
